5ee76a7189a2a174865aece4ace7b4b4d708ac46
anikakash <anik15-14210@diu.edu.bd>
2022-04-08 16:26:42 +0600

    type's of method

--- OOP2_IN_PYTHON/class_function/main.py
+++ OOP2_IN_PYTHON/class_function/main.py
@@ -10,13 +10,22 @@ class SoftwareEniner:
         print(f"{self.name} is coding in {programming_language}")
 
     # with return value
-    def information(self):
-        information=f"name = {self.name}, age = {self.age}, level = {self.level}"
+    # def information(self):
+    #     information=f"name = {self.name}, age = {self.age}, level = {self.level}"
+    #     return information
+
+    #deunder method
+    def __str__(self):
+        information = f"name = {self.name}, age = {self.age}, level = {self.level}"
         return information
 
+    def __eq__(self, other):
+        return self.name == other.name and self.age==other.age
+
 person1 = SoftwareEniner("anik", 22, "fresher", 50000)
+person2 = SoftwareEniner("anik", 22, "fresher", 50000)
 print(person1.name, person1.age, person1.level, person1.salary)
 person1.work("Python")
 print(SoftwareEniner.Company_Name)
-
-print((person1.information()))
+print(person1==person2)
+print(person1)
