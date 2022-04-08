8d030043aea20fd219ca9fed8c1ebc6230211c92
anikakash <anik15-14210@diu.edu.bd>
2022-04-08 14:50:08 +0600

    class and object in pyhton

---
+++ OOP2_IN_PYTHON/Class & Instance Object/main.py
@@ -0,0 +1,12 @@
+class SoftwareEniner:
+    def __init__(self, name, age, level, salary):
+        self.name=name
+        self.age=age
+        self.level=level
+        self.salary=salary
+    def work(self):
+        print(f"{self.name} is working in a project")
+
+person1 = SoftwareEniner("anik", 22, "fresher", 50000)
+print(person1.name, person1.age, person1.level, person1.salary)
+person1.work()
